075830f349930d6fec93673605ef93ce3fa5b567
cedricgau <cedricgautier94@gmail.com>
2021-04-23 17:24:48 +0200

    ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,10 +2,19 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment;
 
 class HomeController{
 
+/**
+ * @var Environment
+ */
+
+    public function __construct($twig){
+        $this->twig = $twig;
+    }
+
     public function index(): Response {
-        return new Response("Salut Cédric");
+        return new Response($this->twig->render('pages/home.html.twig'));
     }
 }
