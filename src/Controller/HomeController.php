0682a2dbc88aedf1ba9995ef140dc5b5275bb38a
cedricgau <cedricgautier94@gmail.com>
2021-04-23 23:54:32 +0200

    ok

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -1,20 +1,24 @@
 <?php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 use Twig\Environment;
 
-class HomeController{
+class HomeController extends AbstractController{
 
-/**
- * @var Environment
+
+    /**
+ *@Route ("/", name="home")
+ *return Response  
+ *@throws \Twig_Error_Loader
+  *@throws \Twig_Error_Runtime
+  *@throws \Twig_Error_Syntax 
+ *
  */
 
-    public function __construct($twig){
-        $this->twig = $twig;
-    }
-
     public function index(): Response {
-        return new Response($this->twig->render('pages/home.html.twig'));
+        return $this->render('pages/home.html.twig');
     }
 }
