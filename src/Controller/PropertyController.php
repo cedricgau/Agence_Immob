d7f9f034912b850586aaed5234c9480f57d43c0d
cedricgau <cedricgautier94@gmail.com>
2021-04-24 14:09:25 +0200

    ok

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Property;
 
 
 
@@ -17,6 +18,20 @@ class PropertyController extends AbstractController{
  */
 
 public function index(): Response{
+
+    $property = new Property();
+    $property->setTitle('Mon premier bien')
+        ->setPrice(200000)
+        ->setRooms(4)
+        ->setBedrooms(3)
+        ->setDescription('une petite description')
+        ->setSurface(60)
+        ->setFloor(1)
+        ->setHeat(1)
+        ->setCity('Montpellier')
+        ->setAdress('15 Boulevard Gambetta')
+        ->setPostalCode('34000');
+        
     return $this->render('property/index.html.twig', ['current_menu'=>'properties']);
 
 }
