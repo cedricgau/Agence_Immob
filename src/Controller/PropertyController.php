89a2df019edcd289c9bcb0e4cd14aed9b4084b60
cedricgau <cedricgautier94@gmail.com>
2021-04-24 22:24:11 +0200

    k

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -5,10 +5,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\Property;
-
-
+use App\Repository\PropertyRepository;
+use Doctrine\Persistence\ObjectManager;
 
 class PropertyController extends AbstractController{
+
+/**
+ * @var PropertyRepository
+ */
+
+    private $repository;
+
+/**
+*@var ObjectManager
+*/
+
+    private $om;
+
+    public function __construct(PropertyRepository $repository) {
+        #$this->om = $om;
+        $this->repository = $repository;
+    }
 
 
 /**
@@ -19,19 +36,31 @@ class PropertyController extends AbstractController{
 
 public function index(): Response{
 
-    $property = new Property();
-    $property->setTitle('Mon premier bien')
-        ->setPrice(200000)
-        ->setRooms(4)
-        ->setBedrooms(3)
-        ->setDescription('une petite description')
-        ->setSurface(60)
-        ->setFloor(1)
-        ->setHeat(1)
-        ->setCity('Montpellier')
-        ->setAdress('15 Boulevard Gambetta')
-        ->setPostalCode('34000');
-        
+    # Saisi et enregistrement dans la base
+
+    #$property = new Property();
+    #$property->setTitle('Mon premier bien')
+    #    ->setPrice(200000)
+    #    ->setRooms(4)
+    #    ->setBedrooms(3)
+    #    ->setDescription('une petite description')
+    #    ->setSurface(60)
+    #    ->setFloor(1)
+    #    ->setHeat(1)
+    #    ->setCity('Montpellier')
+    #    ->setAdress('15 Boulevard Gambetta')
+    #    ->setPostalCode('34000');
+
+    #    $em = $this->getDoctrine()->getManager();
+    #    $em->persist($property);
+    #    $em->flush();
+
+    # Récupération des enregistrements dans la base
+
+    $property = $this->repository->findAllVisible();
+    #$property[0]->setSold(true);
+    #$this->om->flush();
+
     return $this->render('property/index.html.twig', ['current_menu'=>'properties']);
 
 }
