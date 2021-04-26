d963a9fdc0793270f78058948bfe43a70963c85c
cedricgau <cedricgautier94@gmail.com>
2021-04-26 23:25:26 +0200

    a corriger

--- src/Controller/Admin/AdminPropertyController.php
+++ src/Controller/Admin/AdminPropertyController.php
@@ -1,8 +1,11 @@
 <?php
 namespace App\Controller\Admin;
 
+use App\Entity\Property;
 use App\Repository\PropertyRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
 
 class AdminPropertyController extends AbstractController{
 
@@ -16,8 +19,24 @@ class AdminPropertyController extends AbstractController{
         $this->repository=$repository;
     }
 
+    /**
+     * @Route("/admin", name="admin.property.index")
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+
     public function index(){
-        $property = $this->repository->findAll();
-        return $this->render('admin/property/index.html.twig', compact(('properties')));
+        $properties = $this->repository->findAll();
+        return $this->render('admin/property/index.html.twig', compact('properties'));
     }
+
+    /**
+     * @Route("/admin/{id}", name="admin.property.edit")
+     * @param Property $property
+     * @return \Symfony\Component\HttpFoundation\Response
+     */
+
+     public function edit(Property $property){
+
+        return $this->render('admin/property/edit.html.twig', compact('property'));
+     }
 }
