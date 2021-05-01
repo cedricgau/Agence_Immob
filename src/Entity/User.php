3a256d8481c9eaf3ad6386bceaa2be81f78e5284
cedricgau <cedricgautier94@gmail.com>
2021-05-01 15:28:08 +0200

    feat: ajout de l'authentification

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -72,7 +72,7 @@ class User implements UserInterface,\Serializable
 
     public function serialize()
     {
-        return $this->serialize([
+        return serialize([
             $this->id,
             $this->username,
             $this->password
